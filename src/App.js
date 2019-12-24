9adb7060af559906cf7ca35edd705a5ff3f4ad9d
fatih.durna <fatih.durna@IST-FDURNA>
2019-12-24 17:37:35 +0300

    ...
    ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Button from 'rsuite/lib/Button';
 import './App.css';
+import 'rsuite/dist/styles/rsuite-default.css'; // or 'rsuite/dist/styles/rsuite-default.css'
+
 
 function App() {
   return (
     <div className="App">
-     
+     <Button>Hello Worldd</Button>
     </div>
   );
 }
