cc13ba2c2f6b72eb7aca54994a2840705012b514
fdurna <fdfatihdurna@gmail.com>
2020-01-23 12:07:33 +0300

    userlist get data
    userlist get data

--- src/components/userList/UserList.js
+++ src/components/userList/UserList.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 import { Input , Icon } from 'rsuite';
+import axios from 'axios';
 import './UserList.scss'
 
 
 export default class UserList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            users:[]
+        }
+        console.log("constructor");
+    }
+    componentDidMount(){
+        console.log("componentDidMount");
+        axios.get('http://10.16.0.63:5000/api/users')
+        .then(res=> {
+            console.log(res);
+            this.setState({
+                users:res.data.results
+            });
+        })
+    }
     render() {
+        console.log("render");
         return (
             <section className="userList">
                 <div className="messengerSearch">
@@ -22,149 +41,22 @@ export default class UserList extends Component {
                     </div>
                 </div>
                 <div className="usersList">
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/32.jpg" alt="test" />
+                    {
+                        this.state.users.map(
+                            user=>
+                            <div className="item" key={user.id}>
+                                <div className="itemCont">
+                                    <div className="img">
+                                        <img src="https://randomuser.me/api/portraits/women/32.jpg" alt="test" />
+                                    </div>
+                                    <div className="text">
+                                        <h1>{user.name} / {user.username}</h1>
+                                        <p>Hello world! This is a long message</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="text">
-                                <h1>Darlene Steeves</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/men/69.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Maya Barnett</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="itemCont">
-                            <div className="img">
-                                <img src="https://randomuser.me/api/portraits/women/31.jpg" alt="test" />
-                            </div>
-                            <div className="text">
-                                <h1>Sam Henry</h1>
-                                <p>Hello world! This is a long message</p>
-                            </div>
-                        </div>
-                    </div>
+                        )
+                    }
                 </div>
             </section>
         )
